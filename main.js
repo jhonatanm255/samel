741d4fcd1eb11b67bb681e9fd120141a3f5ee1cf
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-05-18 22:21:30 -0400

    ajuste de colores en info experiencia

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-function animateValue(id, start, end, duration) {
-    let startTimestamp = null;
-    const step = (timestamp) => {
-        if (!startTimestamp) startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        document.getElementById(id).textContent = Math.floor(progress * (end - start) + start) + '+';
-        if (progress < 1) {
-            window.requestAnimationFrame(step);
-        }
-    };
-    window.requestAnimationFrame(step);
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                animateValue("experiencia", 0, 10, 3000);
-                animateValue("tatuajes", 0, 130, 3000);
-                animateValue("disenos", 0, 800, 3000);
+                entry.target.classList.add('visible');
                 observer.unobserve(entry.target); // Deja de observar una vez que se ha animado
             }
         });
@@ -25,8 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
         threshold: 0.5 // Inicia la animación cuando el 50% del elemento sea visible
     });
 
-    const infoExperiencia = document.querySelector('.info-experiencia');
-    observer.observe(infoExperiencia);
+    const items = document.querySelectorAll('.hidden');
+    items.forEach(item => {
+        observer.observe(item);
+    });
 });
 
 
