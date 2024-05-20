ca4f4d7502ca61b475b9f9afc9572f760af93102
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-05-20 02:08:51 -0400

    insercion de la seccion de comentarios

--- main.js
+++ main.js
@@ -18,6 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
+// TESTIMONIAL SCRIPT
+$('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:6000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:3
+        },
+    }
+});
 
 
