5c1897fa6e0e4b961f670b46189bfe1337dfd9db
jhonatan Garcia <jhonm21@gmail.com>
2024-05-22 01:08:24 -0400

    incorporacion del panel de administracion al proyecto con la fncion de borrar comentarios

---
+++ admin.js
@@ -0,0 +1,178 @@
+// Función para la animación de los valores de la sección de experiencia
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Deja de observar una vez que se ha animado
+            }
+        });
+    }, {
+        threshold: 0.5 // Inicia la animación cuando el 50% del elemento sea visible
+    });
+
+    const items = document.querySelectorAll('.hidden');
+    items.forEach(item => {
+        observer.observe(item);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dbRef = firebase.database().ref('testimonials');
+
+    // Observador para verificar si hay testimonios al cargar la página
+    dbRef.on('value', function(snapshot) {
+        const noComent = document.querySelector('.no-comentarios');
+        if (!snapshot.exists()) {
+            noComent.style.display = 'block';
+        } else {
+            noComent.style.display = 'none';
+        }
+    });
+
+    // Función para agregar un testimonio
+    function addTestimonial(name, message, rating) {
+        const newTestimonialRef = dbRef.push();
+        newTestimonialRef.set({
+            name: name,
+            message: message,
+            rating: parseInt(rating),
+            timestamp: firebase.database.ServerValue.TIMESTAMP
+        });
+    }
+
+    // Función para abrir la ventana modal del formulario de comentarios
+    const btnModal = document.getElementById('modal');
+    if (btnModal) {
+        btnModal.addEventListener('click', () => {
+            const modalWindow = document.querySelector('.form-testimonial');
+            modalWindow.style.display = 'block';
+        });
+    }
+
+    const btnClosed = document.getElementById('closed');
+    btnClosed.addEventListener('click', () => {
+        const modalWindow = document.querySelector('.form-testimonial');
+        modalWindow.style.display = 'none';
+    });
+
+    // Manejar la selección de estrellas
+    const stars = document.querySelectorAll('.rating .fa-star');
+    stars.forEach(star => {
+        star.addEventListener('click', function() {
+            const value = this.getAttribute('data-value');
+            document.getElementById('rating').value = value; // Actualiza el campo oculto
+            stars.forEach(s => s.classList.remove('selected'));
+            for (let i = 0; i < value; i++) {
+                stars[i].classList.add('selected');
+            }
+        });
+    });
+
+    // Manejar el envío del formulario
+    document.getElementById('send').addEventListener('click', function(e) {
+        e.preventDefault();
+        const name = document.getElementById('name').value.trim();
+        const message = document.getElementById('msj').value.trim();
+        const rating = document.getElementById('rating').value.trim();
+
+        console.log(`Name: ${name}, Message: ${message}, Rating: ${rating}`);
+
+        if (name && message && rating) {
+            addTestimonial(name, message, rating);
+            document.getElementById('name').value = '';
+            document.getElementById('msj').value = '';
+            document.getElementById('rating').value = '';
+            stars.forEach(star => star.classList.remove('selected'));
+            swal("Enviado", "", "success");
+            const modalWindow = document.querySelector('.form-testimonial');
+                modalWindow.style.display = 'none';
+        } else {
+            swal("Por favor, complete los campos", "", "error").then(() => {
+                const modalWindow = document.querySelector('.form-testimonial');
+                modalWindow.style.display = 'block';
+            });
+        }
+    });
+
+    // Función para cargar testimonios y configurar el carrusel
+    function loadTestimonials() {
+        dbRef.on('value', (snapshot) => {
+            const testimonialsContainer = $('.testimonials-container');
+            testimonialsContainer.owlCarousel('destroy'); // Destruir el carrusel existente
+            testimonialsContainer.html(''); // Limpiar contenido previo
+
+            snapshot.forEach((childSnapshot) => {
+                const childData = childSnapshot.val();
+                const date = new Date(childData.timestamp).toLocaleString();
+                const rating = childData.rating;
+
+                // Crear estrellas según la calificación
+                let stars = '';
+                for (let i = 1; i <= 5; i++) {
+                    stars += `<i class="fa${i <= rating ? ' fas' : ' far'} fa-star"></i>`;
+                }
+
+                const testimonialCard = `
+                    <div class="testimonial-card">
+                        <main class="test-card-body">
+                            <div class="quote">
+                                <i class="fa fa-quote-left"></i>
+                                <h2>${childData.name}</h2>
+                                <button class="closed" data-id="${childSnapshot.key}">X</button> <!-- Añadir un atributo de datos para almacenar el ID del comentario -->
+                                </div>
+                            <p>${childData.message}</p>
+                            <div class="ratings">
+                                <div class='stars'>${stars}</div>
+                                <div class="date">${date}</div>
+                            </div>
+                        </main>
+                    </div>
+                `;
+                testimonialsContainer.append(testimonialCard);
+            });
+
+             // Agregar manejador de eventos al botón "closed"
+            $('.closed').on('click', function() {
+                const comentarioID = $(this).data('id'); // Obtener el ID del comentario
+                eliminarComentario(comentarioID); // Llamar a la función eliminarComentario con el ID del comentario
+             });
+
+             // Función para eliminar un comentario de Firebase
+            function eliminarComentario(comentarioID) {
+                 if (confirm('¿Estás seguro de que deseas eliminar este comentario?')) {
+                 dbRef.child(comentarioID).remove(); // Eliminar el comentario de la base de datos
+                 }
+    }
+
+
+            // Inicializar el carrusel después de cargar los testimonios
+            testimonialsContainer.owlCarousel({
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 6000,
+                margin: 10,
+                nav: true,
+                navText: ["<i class='fa-solid fa-arrow-left'></i>", "<i class='fa-solid fa-arrow-right'></i>"],
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: true
+                    },
+                    600: {
+                        items: 2,
+                        nav: true
+                    },
+                    768: {
+                        items: 1,
+                        nav: true
+                    },
+                }
+            });
+        });
+    }
+
+    // Cargar testimonios al iniciar la página
+    loadTestimonials();
+});
+
